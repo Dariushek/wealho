d8b159b4a17b0d5750ab9cc04ea7b63f8766c94e
Dariuszek <dariuszborawskidb@gmail.com>
2022-07-12 19:11:50 +0200

    Revert change texts
    revert change texts

--- src/Wealho.WebApi/Controllers/WeatherForecastController.cs
+++ src/Wealho.WebApi/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Mroźno", "Wietrznie", "Ciepło", "Gorąco"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
