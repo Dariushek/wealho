31bd8b926850c49dabb0bde976817cfda1b59d64
Dariuszek <dariuszborawskidb@gmail.com>
2022-07-13 17:22:30 +0200

    Web App configuration

---
+++ src/Wealho.WebApp/Configuration/Configuration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace Wealho;
+
+public static class Configuration
+{
+    public static void ConfigureServices(this WebAssemblyHostBuilder builder)
+    {
+        string baseUrl = builder.Configuration["ApiBaseUrl"] ?? throw new ApplicationException("Missing configuration.");
+        builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new(baseUrl)});
+    }
+}
--- src/Wealho.WebApp/Program.cs
+++ src/Wealho.WebApp/Program.cs
@@ -1,11 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Wealho.WebApp;
+using Wealho;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri("https://wealho-api.lemonflower-897dced6.westeurope.azurecontainerapps.io")});
+builder.ConfigureServices();
 
 await builder.Build().RunAsync();
