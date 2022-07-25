200e8cc6cdfdf616e8214a62fdef7478574126eb
Dariuszek <dariuszborawskidb@gmail.com>
2022-07-25 10:27:50 +0200

    Replace bootstrap with Bulma

--- src/Wealho.Ui/Configuration/Configuration.cs
+++ src/Wealho.Ui/Configuration/Configuration.cs
@@ -7,6 +7,8 @@ public static class Configuration
     public static void ConfigureServices(this WebAssemblyHostBuilder builder)
     {
         string baseUrl = builder.Configuration["ApiBaseUrl"] ?? throw new ApplicationException("Missing configuration.");
-        builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new(baseUrl)});
+        builder.Services
+            .AddScoped(sp => new HttpClient {BaseAddress = new(baseUrl)})
+            ;
     }
 }
